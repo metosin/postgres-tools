557f74e2a1451806bca4b90594c2b14938a4024c
Lassi Immonen <lassi.immonen@metosin.fi>
2016-10-24 02:56:28 +0300

    Add options passing to jdbc and clean up repetitious code

--- src/postgres_tools/core.clj
+++ src/postgres_tools/core.clj
@@ -3,17 +3,8 @@
     [clojure.string :as str]
     [jdbc.core :as jdbc]))
 
-;
-;(defn query
-;  ([ctx qry] (query identity ctx qry))
-;  ([tf ctx qry]
-;   (-> (jdbc/query (:db ctx) (sql/format qry)) tf)))
-
-
 ;;
-;; New WIP versions of insert, update and upsert using real
-;; Postgres backed implementations
-;;
+;; Private functions
 ;;
 
 (defn- underscore [x] (str/replace x #"-" "_"))
@@ -51,9 +42,6 @@
          (str " RETURNING "
               (str/join ", " (map identifier-to ret-columns))))))
 
-;insert into foo(a,b) values ('jee',2)
-;on conflict (b) do update set a = excluded.a;
-
 (defn- upsert-sql [identity-columns value-columns]
   (str " ON CONFLICT ("
        (str/join ", " (map identifier-to identity-columns))
@@ -64,6 +52,15 @@
                                column))
                            value-columns))))
 
+(defn- fetch-or-execute [dbspec sqlvec returning-columns options]
+  (println (prn-str sqlvec))
+  (with-open [conn (jdbc/connection dbspec)]
+    (jdbc/atomic conn
+      (if returning-columns
+        (let [options (merge {:identifiers identifier-from} options)]
+          (jdbc/fetch conn sqlvec options))
+        (jdbc/execute conn sqlvec options)))))
+
 ;;
 ;; Public api
 ;;
@@ -71,8 +68,11 @@
 (defn insert!
   "Jdbc insert. Data is list of maps. Returned columns
   can be provided as a vector of column names."
-  ([dbspec table-name data] (insert! dbspec table-name data nil))
+  ([dbspec table-name data]
+   (insert! dbspec table-name data nil nil))
   ([dbspec table-name data returning-columns]
+   (insert! dbspec table-name data returning-columns nil))
+  ([dbspec table-name data returning-columns options]
    {:pre [(sequential? data) (map? (first data))]}
 
    (let [columns (keys (first data))
@@ -84,20 +84,17 @@
          sqlvec (into [sql] (for [row data
                                   k columns]
                               (get row k)))]
-     (println (prn-str sqlvec))
-     (with-open [conn (jdbc/connection dbspec)]
-       (jdbc/atomic conn
-         (if returning-columns
-           (jdbc/fetch conn sqlvec {:identifiers identifier-from})
-           (jdbc/execute conn sqlvec)))))))
+     (fetch-or-execute dbspec sqlvec returning-columns options))))
 
 (defn update!
   "Jdbc update. Data is a map of new column values and
   identity is a map of column values on which row(s) should be updated.
   Returned columns can be provided as a vector of column names."
   ([dbspec table-name data-map identity-map]
-   (update! dbspec table-name data-map identity-map nil))
+   (update! dbspec table-name data-map identity-map nil nil))
   ([dbspec table-name data-map identity-map returning-columns]
+   (update! dbspec table-name data-map identity-map returning-columns nil))
+  ([dbspec table-name data-map identity-map returning-columns options]
    {:pre [(map? data-map) (map? identity-map)]}
 
    (let [sql (update-sql table-name
@@ -105,12 +102,7 @@
                          (keys identity-map)
                          returning-columns)
          sqlvec (into [sql] (concat (vals data-map) (vals identity-map)))]
-     (println (prn-str sqlvec))
-     (with-open [conn (jdbc/connection dbspec)]
-       (jdbc/atomic conn
-         (if returning-columns
-           (jdbc/fetch conn sqlvec {:identifiers identifier-from})
-           (jdbc/execute conn sqlvec)))))))
+     (fetch-or-execute dbspec sqlvec returning-columns options))))
 
 (defn upsert!
   "Inserts using Postgresql insert with upsert semantics.
@@ -119,8 +111,10 @@
   Data rows get either updated or inserted based on identity search.
   Returned columns can be provided as a vector of column names."
   ([dbspec table-name data identity-columns]
-   (upsert! dbspec table-name data identity-columns nil))
+   (upsert! dbspec table-name data identity-columns nil nil))
   ([dbspec table-name data identity-columns returning-columns]
+   (upsert! dbspec table-name data identity-columns returning-columns nil))
+  ([dbspec table-name data identity-columns returning-columns options]
    {:pre [(sequential? data) (map? (first data))]}
 
    (let [columns (keys (first data))
@@ -133,21 +127,19 @@
          sqlvec (into [sql] (for [row data
                                   k columns]
                               (get row k)))]
-     (println (prn-str sqlvec))
-     (with-open [conn (jdbc/connection dbspec)]
-       (jdbc/atomic conn
-         (if returning-columns
-           (jdbc/fetch conn sqlvec {:identifiers identifier-from})
-           (jdbc/execute conn sqlvec)))))))
+     (fetch-or-execute dbspec sqlvec returning-columns options))))
 
 (defn query
-  [dbspec sql-statement]
-  (with-open [conn (jdbc/connection dbspec)]
-    (jdbc/fetch conn sql-statement {:identifiers identifier-from})))
+  ([dbspec sql-statement] (query dbspec sql-statement nil))
+  ([dbspec sql-statement options]
+   (let [options (merge {:identifiers identifier-from} options)]
+     (with-open [conn (jdbc/connection dbspec)]
+       (jdbc/fetch conn sql-statement options)))))
 
 (defn execute!
-  [dbspec sql-statements]
-  (with-open [conn (jdbc/connection dbspec)]
-    (jdbc/atomic conn
-      (doseq [s sql-statements]
-        (jdbc/execute conn s)))))
+  ([dbspec sql-statements] (execute! dbspec sql-statements nil))
+  ([dbspec sql-statements options]
+   (with-open [conn (jdbc/connection dbspec)]
+     (jdbc/atomic conn
+       (doseq [s sql-statements]
+         (jdbc/execute conn s))))))
